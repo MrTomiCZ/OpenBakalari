7d3f4e053576ca5fa7418b9c0ca7ffc00132615f
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-11-13 16:13:16 +0100

    Add error handling and info endpoint to app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const xprs = express();
+const PORT = 8453;
 
 xprs.listen(PORT, function (err) {
-    if (err) console.error("")
+    if (err) console.error("❌ Express tě nechce odposlouchávat:",err);
+    console.log("✅ Express tě odposlouchává na portu "+PORT);
+});
+
+xprs.get("/info", (req, res) => {
+    res.send("200 OK");
 });
